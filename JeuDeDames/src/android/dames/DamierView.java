271b91e19b86695df971bb7d7330136a65bc2106
thomas.souvignet <thomas.souvignet@gmail.com>
2011-03-02 21:52:18 +0000

    Ajout des fichiers nécessaires à la gestion du damier : - PlateauView - DamierView - Layout

---
+++ JeuDeDames/src/android/dames/DamierView.java
@@ -0,0 +1,23 @@
+package android.dames;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class DamierView extends PlateauView {
+
+	public DamierView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DamierView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DamierView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ JeuDeDames/src/android/dames/PlateauView.java
@@ -0,0 +1,24 @@
+package android.dames;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PlateauView extends View {
+
+	public PlateauView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlateauView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlateauView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
