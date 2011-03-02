dfedf0dc61fea1fb75c0c5730523dc31dab333f3
fylhan <fylhan@f49b6cd7-561e-1f21-d332-107d58c8169e>
2011-03-02 20:46:21 +0000

    Test d'écriture d'un XML

--- trunk/Serveur/index.php
+++ trunk/Serveur/index.php
@@ -1,3 +1,11 @@
 <?php
-echo 'Hello '.(isset($_GET['data']) && NULL != $_GET['data'] ? $_GET['data'] : 'world').'!';
+header("Content-Type: text/xml" );
+echo '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n".'
+<partie>'."\n".'
+	<id>1</id>'."\n".'
+	<numeroTour>3</numeroTour>'."\n".'
+	<pionJoue>'."\n".'
+		<id>2</id>'."\n".'
+	</pionJoue>'."\n".'
+</partie>';
 ?>
