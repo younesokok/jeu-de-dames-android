693add7dcda38d8305c7d879c1bccadbb495ff31
fylhan <fylhan@f49b6cd7-561e-1f21-d332-107d58c8169e>
2011-03-03 20:25:35 +0000

    Ajout d'une classe gérant la communication avec le serveur

--- Serveur/index.php
+++ Serveur/index.php
@@ -1,16 +1,20 @@
 <?php
-header("Content-Type: text/xml" );
-echo '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n".'
-<tour>'."\n".'
-	<idPartie>1</idPartie>'."\n".'
-	<numero>3</numero>'."\n".'
-	<deplacementsPionJoue>
-		<deplacement positionIn="1" positionOut="4" />
-		<deplacement positionIn="4" positionOut="2" />
-	</deplacementsPionJoue>'."\n".'
-	<pionsManges>'."\n".'
-		<pion position="3" />'."\n".'
-		<pion position="5" />'."\n".'
-	</pionsManges>'."\n".'
-</tour>';
+if (isset($_GET['sens']) && NULL != $_GET['sens'] && '' != $_GET['sens']) {
+	if ('receive' == $_GET['sens']) {
+		header("Content-Type: text/xml" );
+		echo '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n".'
+		<tour>'."\n".'
+			<idPartie>1</idPartie>'."\n".'
+			<numero>3</numero>'."\n".'
+			<deplacementsPionJoue>
+				<deplacement positionIn="1" positionOut="4" />
+				<deplacement positionIn="4" positionOut="2" />
+			</deplacementsPionJoue>'."\n".'
+			<pionsManges>'."\n".'
+				<pion position="3" />'."\n".'
+				<pion position="5" />'."\n".'
+			</pionsManges>'."\n".'
+		</tour>';
+	}
+}
 ?>
