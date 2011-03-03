22ca936edb538c42251b46495040a816cd4c247b
thomas.souvignet@gmail.com <thomas.souvignet@gmail.com@f49b6cd7-561e-1f21-d332-107d58c8169e>
2011-03-03 22:37:00 +0000

    Damier présent (images à changer).
    Liste de pion en cours de mise en place.

--- trunk/JeuDeDames/src/android/dames/PlateauView.java
+++ trunk/JeuDeDames/src/android/dames/PlateauView.java
@@ -9,11 +9,12 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class PlateauView extends View {
 
 	/* Parametres des cases */
-	private static int mTailleCase;
+	private int mTailleCase = 30;
 	private static int mNbCases = 10;
 	private static int mMarge = 0;
 	/* Offset du début de damier */
@@ -21,26 +22,17 @@ public class PlateauView extends View {
 	private static int mYOffset;
 	/* Tableau de cases */
 	private int[][] mTableauCases = new int[10][10]; 
-	/* Tableau contenant les cases */
-    private Bitmap[] mCasesTab; 
+	/* Tableau contenant les différentes types de cases */
+    private Bitmap[] mTypeCases; 
 
     private final Paint mPaint = new Paint();
 
 	public PlateauView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		
-		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PlateauView);
-		mTailleCase = a.getInt(R.styleable.PlateauView_tailleCase, 20);
-		a.recycle();
-		
 	}
 
 	public PlateauView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
-		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PlateauView);
-		mTailleCase = a.getInt(R.styleable.PlateauView_tailleCase, 20);
-		a.recycle();
 	}
 
 	/**
@@ -48,7 +40,7 @@ public class PlateauView extends View {
 	 * @param nbTypeCases
 	 */
     public void resetCases(int nbTypeCases) {
-    	mCasesTab = new Bitmap[nbTypeCases];
+    	mTypeCases = new Bitmap[nbTypeCases];
     }
 	
 	/**
@@ -61,7 +53,7 @@ public class PlateauView extends View {
         Canvas canvas = new Canvas(bitmap);
         sprite.setBounds(0, 0, mTailleCase, mTailleCase);
         sprite.draw(canvas);
-        mCasesTab[typeCase] = bitmap;
+        mTypeCases[typeCase] = bitmap;
 	}
     
 	@Override
@@ -71,7 +63,7 @@ public class PlateauView extends View {
         for (int x = 0; x < mNbCases; x += 1) {
             for (int y = 0; y < mNbCases; y += 1) {
                 if (mTableauCases[x][y] > 0) {
-                    canvas.drawBitmap(mCasesTab[mTableauCases[x][y]], 
+                    canvas.drawBitmap(mTypeCases[mTableauCases[x][y]], 
                     		mXOffset + x * mTailleCase,
                     		mYOffset + y * mTailleCase,
                     		mPaint);
@@ -94,15 +86,15 @@ public class PlateauView extends View {
         clearCases();
 	}
 
-	private void clearCases() {
+	public void clearCases() {
         for (int x = 0; x < mNbCases; x++) {
             for (int y = 0; y < mNbCases; y++) {
-                setTile(((x+y)%2)+1, x, y);
+                setCase(((x+y)%2)+1, x, y);
             }
         }
 	}
 
-	private void setTile(int type, int x, int y) {
+	public void setCase(int type, int x, int y) {
         mTableauCases[x][y] = type;		
 	}
 }
