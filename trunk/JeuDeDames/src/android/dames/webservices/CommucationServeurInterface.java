9325f8533b0f29fa05537299931874a14d32f27d
fylhan <fylhan@f49b6cd7-561e-1f21-d332-107d58c8169e>
2011-03-03 22:40:57 +0000

    passage en utf-8

--- trunk/JeuDeDames/src/android/dames/webservices/CommucationServeurInterface.java
+++ trunk/JeuDeDames/src/android/dames/webservices/CommucationServeurInterface.java
@@ -8,20 +8,20 @@ import android.dames.Tour;
 public interface CommucationServeurInterface {
 	/**
 	 * Retourne l'URL du service Web
-	 * @param url Url sans les paramètres
-	 * @param params Liste des paramètrs (name=valeur)
+	 * @param url Url sans les paramÃ¨tres
+	 * @param params Liste des paramÃ¨trs (name=valeur)
 	 * @return l'URL du service Web
 	 */
 	public URL buildUrl(String url, Map<String, String> params);
 	/**
-	 * Retourne le tour en cours à partir du serveur
+	 * Retourne le tour en cours Ã  partir du serveur
 	 * @return tour courant
 	 */
 	public Tour getTourCourant();
 	/**
-	 * Envoi un nouveau tour terminé au serveur
-	 * @param tour Tour terminé
-	 * @return Tour courant (si tout s'est bien passé égale au tour terminé)
+	 * Envoi un nouveau tour terminÃ© au serveur
+	 * @param tour Tour terminÃ©
+	 * @return Tour courant (si tout s'est bien passÃ© Ã©gale au tour terminÃ©)
 	 */
 	public Tour sendTourFini(Tour tour);
 }
