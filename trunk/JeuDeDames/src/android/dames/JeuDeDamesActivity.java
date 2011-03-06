6eb1357d7be788ee153c1bbb205303688c5b273d
thomas.souvignet@gmail.com <thomas.souvignet@gmail.com@f49b6cd7-561e-1f21-d332-107d58c8169e>
2011-03-06 18:37:59 +0000

    deplacement ok. gestion de la validation par le menu

--- trunk/JeuDeDames/src/android/dames/JeuDeDamesActivity.java
+++ trunk/JeuDeDames/src/android/dames/JeuDeDamesActivity.java
@@ -2,6 +2,8 @@ package android.dames;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class JeuDeDamesActivity extends Activity {
@@ -54,5 +56,24 @@ public class JeuDeDamesActivity extends Activity {
 		/* On stocke l'etat du Damier */
         outState.putBundle(mBundleKey, mDamierView.saveState());
 	}
-
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	menu.add("Valider");
+    	menu.add("Simul retour attente");
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if(item.getTitle()=="Valider") {
+    		mDamierView.setEtat(mDamierView.VALID);
+    		mDamierView.updateGame();
+    	}
+    	if(item.getTitle()=="Simul retour attente") {
+    		mDamierView.setEtat(mDamierView.SELECT);
+    		mDamierView.updateGame();
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
 }
